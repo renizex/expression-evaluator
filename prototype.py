0c8e723799827e49dcfca4bc7e1d3315a6a8c062
renizex <xr4tygd@gmail.com>
2026-07-12 15:58:08 +0300

    initial prototype

---
+++ prototype.py
@@ -0,0 +1,66 @@
+def plus(a, b):
+    return a+b
+
+def minus(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return a/b
+
+operations = {
+    "+": plus,
+    "-": minus,
+    "*": multiply,
+    "/": divide
+}
+
+def main():
+    while True:
+        answer = ""
+        digits = []
+        operators = []
+        print("введи выражение")
+        result = input("> ").strip()
+        if result == "":
+            print("ошибка")
+            continue
+        separation = result.split()
+        for expression in separation:
+            if expression.isdigit():
+                digits.append(int(expression))
+            elif expression in ["+", "-", "*", "/"]:
+                operators.append(expression)
+            else:
+                print("ты ввел какое то гавно")
+                continue
+
+        while operators:
+            op1 = len(operators) + 1
+            op2 = len(operators) + 1
+            op3 = len(operators) + 1
+            op4 = len(operators) + 1
+            if "*" in operators:
+                op2 = operators.index("*")
+            if "/" in operators:
+                op1 = operators.index("/")
+            if "-" in operators:
+                op4 = operators.index("-")
+            if "+" in operators:
+                op3 = operators.index("+")
+            op = min(op1, op2)
+            if op ==  len(operators) + 1:
+                op = min(op3, op4)
+            n1 = digits[op]
+            n2 = digits[op + 1]
+            if operators[op] == "/" and n2 == 0:
+                print("на ноль делить увы нельзя")
+                break
+            answer = operations[operators[op]](n1, n2)
+            digits[op:op + 2] = [answer]
+            operators.pop(op)
+        print(answer)
+
+main()
