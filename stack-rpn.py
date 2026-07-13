45ca14b98cdfaa8e63e70335f9bd888cafc154a3
renizex <xr4tygd@gmail.com>
2026-07-13 22:14:41 +0300

    v1.0: initial stable stack-rpn build

--- stack-rpn.py
+++ stack-rpn.py
@@ -8,7 +8,9 @@ def multiply(a, b):
     return a*b
 
 def divide(a, b):
-    return a/b
+    if b != 0:
+        return a/b
+    raise ValueError("division by zero")
 
 operations = {
     "+": plus,
@@ -19,47 +21,46 @@ operations = {
 
 def main():
     while True:
-        digits_count = 0
-        operators_count = 0
-        error_flag = False
-        stack = []
-        answer = input("> ")
-        if not answer or answer.strip() == "":
-            print("you didn't write anything")
-            continue
-        separation = answer.split()
-        if len(separation) < 3:
-            print("expression is too short")
-            continue
-        for token in separation:
-            if token.isdigit():
-                digits_count += 1
-            else:
-                operators_count += 1
-        if digits_count != operators_count + 1:
-            print("this expression logically incorrect")
-            continue
-        for token in separation:
-            if token.isdigit():
-                stack.append(int(token))
-            else:
-                if len(stack) > 1:
-                    second_number = stack.pop()
-                    first_number = stack.pop()
-                    temporary_result = operations[token](first_number, second_number)
-                    stack.append(temporary_result)
+        try:
+            digits_count = 0
+            operators_count = 0
+            error_flag = False
+            stack = []
+            answer = input("> ")
+            if not answer or answer.strip() == "":
+                print("you didn't write anything")
+                continue
+            separation = answer.split()
+            for token in separation:
+                if token.isdigit():
+                    digits_count += 1
+                    stack.append(int(token))
                 else:
-                    print("not enough numbers in stack")
-                    error_flag = True
-                    break
-        if error_flag:
+                    operators_count += 1
+                    if len(stack) > 1:
+                        second_number = stack.pop()
+                        first_number = stack.pop()
+                        if token in operations:
+                            temporary_result = operations[token](first_number, second_number)
+                            stack.append(temporary_result)
+                        else:
+                            print("unknown operator")
+                            error_flag = True
+                            break
+                    else:
+                        print("not enough numbers in stack")
+                        error_flag = True
+                        break
+            if digits_count != operators_count + 1 and not error_flag:
+                print("this expression is logically incorrect")
+                error_flag = True
+            if error_flag:
+                continue
+            else:
+                final_result = stack.pop()
+                print(f"your answer: {final_result}")
+        except ValueError as msg:
+            print(msg)
             continue
-        elif len(stack) > 1:
-            print("invalid expression")
-            continue
-        else:
-            final_result = stack.pop()
-            print(f"your answer: {final_result}")
-
 
 main()
